9e78d6ae719e99a14f9875c6e191fe6b9ee7c357
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2021-10-02 17:13:48 -0500

    car race project

--- js/script.js
+++ js/script.js
@@ -184,9 +184,12 @@ $(function()
         });
     });
     
-    /* ----------------
-       jQUERY CSS
-       ---------------- */
+  
+
+
+
+      // jQUERY CSS
+
     
     $('#circle2').css({
         'background':'#8a8d22',
@@ -198,6 +201,99 @@ $(function()
         'width':'140px'
     }).addClass('circleShape');
 
+
+
+
+
+
+
+
+
+     
+
+    // jQUERY CAR RACR 2.0
+    
+    
+    // Click the GO button
+    $('#go').click(function() {
+        
+        
+        //checks to see if a car has won the race
+        function checkIfComplete() {
+            
+            if( isComplete == false ) {
+                isComplete = true;
+            } else {
+                place = 'second';
+            }
+            
+        }
+        
+        // get the width of the cars
+        var carWidth = $('#car1').width();
+        
+        // get the width of the racetrack
+        var raceTrackWidth = $(window).width() - carWidth;
+        
+        // generate a random # between 1 and 5000
+        
+        var raceTime1 = Math.floor( (Math.random() * 5000) + 1 );
+        var raceTime2 = Math.floor( (Math.random() * 5000) + 1 );
+        
+        // set a flag variable to FALSE by default
+        var isComplete = false;
+        
+        // set a flag variable to FIRST by default
+        var place = 'first';
+        
+
+
+
+        // animate car 1
+        $('#car1').animate({
+        
+            // move the car width of the racetrack
+            left: raceTrackWidth
+            
+        }, raceTime1, function() {
+            
+            // animation is complete
+            
+
+            // run a function
+            checkIfComplete();
+            
+            // give some text feedback in the race info box
+            $('#raceInfo1 span').text( 'Finished in ' + place + ' place and clocked in at ' + raceTime1 + ' milliseconds!' );
+            
+        });
+        
+        // animate car 2
+        $('#car2').animate({
+        
+            // move the car width of the racetrack
+            left: raceTrackWidth
+            
+        }, raceTime2, function() {
+            
+            // animation is complete
+            
+            // run a function
+            checkIfComplete();
+            
+            // give some text feedback in the race info box
+            $('#raceInfo2 span').text( 'Finished in ' + place + ' place and clocked in at ' + raceTime2 + ' milliseconds!' );
+            
+        });
+        
+    });
+    
+    // reset
+    $('#reset').click(function() {
+        $('.car').css('left','0');
+        $('.raceInfo span').text('');
+    });
+    
 });
     
 
